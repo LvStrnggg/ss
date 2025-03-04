bf7b4ef20ba5b0bce33f83c7803c756377d27675
LvStrng <126511387+LvStrnggg@users.noreply.github.com>
2025-03-04 21:17:47 +0200

    cleanup

--- sstool/main.cpp
+++ sstool/main.cpp
@@ -80,36 +80,34 @@ int main() {
 }
 
 std::vector<void*> pattern_scan(HANDLE hProcess, const std::vector<std::string_view>& patterns) {
-    SYSTEM_INFO sys_info {};
+    SYSTEM_INFO sys_info;
     GetSystemInfo(&sys_info);
 
-    std::vector<void*> results {};
-    MEMORY_BASIC_INFORMATION memInfo {};
-    uint8_t* address = (uint8_t*) sys_info.lpMinimumApplicationAddress;
+    std::vector<void*> results;
+    MEMORY_BASIC_INFORMATION memInfo;
+    uint8_t* address = static_cast<uint8_t*>(sys_info.lpMinimumApplicationAddress);
 
     while (address < sys_info.lpMaximumApplicationAddress && VirtualQueryEx(hProcess, address, &memInfo, sizeof(memInfo))) {
-
-        if (memInfo.State == MEM_COMMIT && (memInfo.Protect & PAGE_READWRITE || memInfo.Protect & PAGE_EXECUTE_READWRITE) &&
+        if (memInfo.State == MEM_COMMIT && (memInfo.Protect & (PAGE_READWRITE | PAGE_EXECUTE_READWRITE)) &&
             memInfo.Type == MEM_PRIVATE) {
 
             std::vector<uint8_t> buffer(memInfo.RegionSize);
             SIZE_T bytesRead;
             if (ReadProcessMemory(hProcess, memInfo.BaseAddress, buffer.data(), buffer.size(), &bytesRead)) {
-                std::string_view view((char*) buffer.data(), bytesRead);
+                std::string_view view(reinterpret_cast<char*>(buffer.data()), bytesRead);
 
-                for (const std::string_view& pattern : patterns) {
-                    for (size_t pos = view.find(pattern); pos != std::string_view::npos; pos = view.find(pattern, pos + 1)) {
-                        void* found = (uint8_t*) memInfo.BaseAddress + pos;
-
-                        std::cout << "[*] Found string " << pattern << " at " << std::hex << std::uppercase << (uintptr_t) found << "\n";
-
+                for (const auto& pattern : patterns) {
+                    size_t pos = 0;
+                    while ((pos = view.find(pattern, pos)) != std::string_view::npos) {
+                        void* found = static_cast<uint8_t*>(memInfo.BaseAddress) + pos;
+                        std::cout << "[*] Found string " << pattern << " at " << std::hex << std::uppercase << reinterpret_cast<uintptr_t>(found) << "\n";
                         results.push_back(found);
+                        ++pos;
                     }
                 }
             }
         }
-
-        address = (uint8_t*) memInfo.BaseAddress + memInfo.RegionSize;
+        address = static_cast<uint8_t*>(memInfo.BaseAddress) + memInfo.RegionSize;
     }
     return results;
 }
